584e71ace80430e8e7cea81ac9428809994e7cca
p4pranav <pranavrai07@gmail.com>
2024-02-07 22:52:18 +0530

    fixing test cases

--- src/component/AddTodo/AddTodo.test.js
+++ src/component/AddTodo/AddTodo.test.js
@@ -3,6 +3,8 @@ import {render, screen, fireEvent} from '@testing-library/react'
 import AddTodo from './AddTodo'
 
 
+jest.mock('./AddTodo.css', () => ({}));
+jest.mock('../ListTodoItems/ListTodoItem.css', () => ({}));
 
 describe('AddTodo.js', () => {
     it('Add todo button should be disabled if input field has no text', () => {
@@ -10,6 +12,14 @@ describe('AddTodo.js', () => {
         const button = screen.getByText('Add Todo')
         const inputField = screen.getByPlaceholderText('Enter new todo');
         fireEvent.change(inputField, { target: { value: '' } });
-        expect(button).toBeDisabled()
+        expect(button).toHaveProperty('disabled', true)
+    })
+
+    it('Add todo button should not be disabled if input field has some text', () => {
+        render(<AddTodo/>)
+        const button = screen.getByText('Add Todo')
+        const inputField = screen.getByPlaceholderText('Enter new todo');
+        fireEvent.change(inputField, { target: { value: 'Todo 1' } });
+        expect(button).toHaveProperty('disabled', false)
     })
 })
